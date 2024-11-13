1c4c150a4babcdc409a8a84119dd48acdfef8c67
hamsous <hsoussi002@gmail.com>
2024-11-13 21:56:45 +0000

    small change to add side bar in

--- aml-ui/src/app/app.component.ts
+++ aml-ui/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {RouterLink, RouterModule} from "@angular/router";
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatSidenavModule, RouterLink, RouterModule, MatIconModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
