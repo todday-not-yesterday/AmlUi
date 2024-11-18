20eac0f08ccefbdb10ac4d6777853f34eb225cde
lawrence_whalley4 <lawrence@churchoverhouse.co.uk>
2024-11-18 18:21:48 +0000

    version++

--- aml-ui/src/app/app.routes.ts
+++ aml-ui/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { UserMediaComponent } from './components/user-media/user-media.component';
+import {MediaReturnComponent} from './components/Media-Return/Media-Return.component';
 
 export const routes: Routes = [
-    {path: 'user-media', component: UserMediaComponent}
+    {path: 'user-media', component: UserMediaComponent},
+    {path: 'Media-Return', component: MediaReturnComponent}
 ];
